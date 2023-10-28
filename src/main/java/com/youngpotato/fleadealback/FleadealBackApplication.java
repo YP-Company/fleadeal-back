42f30ab1a7d02ddf02e0b9746bb005ca14555f3e
wujin <woojin97318@gmail.com>
2023-10-28 20:27:01 +0900

    Initial commit

---
+++ src/main/java/com/youngpotato/fleadealback/FleadealBackApplication.java
@@ -0,0 +1,13 @@
+package com.youngpotato.fleadealback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleadealBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleadealBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngpotato/fleadealback/FleadealBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngpotato.fleadealback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleadealBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
